1e82c2d622a349875f2a0527b0da8a759ecf6e61
netotest <stepanmokan@ya.ru>
2024-03-16 21:55:30 +0300

    InitialCommit

---
+++ src/main/java/org/netology/Main.java
@@ -0,0 +1,16 @@
+package org.netology;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {
+                "Бладшот",
+                "Вперед",
+                "Отель <Белград>",
+                "Джентельмены",
+                "Человек-невидимка",
+                "Тролли.Мировой тур",
+                "Номер один"
+        };
+    }
+}
